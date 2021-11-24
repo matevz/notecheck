cbd736fef71d47ca900637a821007f44b3ba44d0
Matevž Jekovec <matevz.jekovec@gmail.com>
2021-11-24 08:55:00 +0100

    urls: Redirect root favicon.ico to favicon.ico in static dir

--- notecheck/urls.py
+++ notecheck/urls.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path('favicon.ico/', RedirectView.as_view(url=settings.STATIC_URL + 'notecheck/favicon.ico')),
     path('', views.index, name='index'),
     path('<str:token>/', views.submission, name='submission'),
     path('<str:token>/<int:submission_id>/', views.submission, name='submission'),
